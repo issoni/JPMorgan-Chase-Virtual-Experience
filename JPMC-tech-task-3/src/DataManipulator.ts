f380dde50bfa39fabb130153b2717d1eb2040fa4
Ishika Soni <68482663+issoni@users.noreply.github.com>
2021-01-16 22:59:47 -0500

    Task 3

---
+++ JPMC-tech-task-3/src/DataManipulator.ts
@@ -0,0 +1,32 @@
+import { ServerRespond } from './DataStreamer';
+
+export interface Row {
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
+}
+
+
+export class DataManipulator {
+  static generateRow(serverRespond: ServerRespond[]): Row { //single row object
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2; //stock abc
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2; //stock def
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+        serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined, //has a value if the threshold is passed by the ratio
+    };
+  } 
+}
